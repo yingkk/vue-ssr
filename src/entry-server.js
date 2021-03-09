0a4fe046f3b2a7fb00d892c78d432587afe04add
ying <932126394@qq.com>
2021-03-09 17:34:15 +0800

    commit

--- src/entry-server.js
+++ src/entry-server.js
@@ -1,9 +1,7 @@
 import { createApp } from './main'
 
 export default context => {
-  // 因为有可能会是异步路由钩子函数或组件，所以我们将返回一个 Promise，
-    // 以便服务器能够等待所有的内容在渲染前，
-    // 就已经准备就绪。
+  // 因为有可能会是异步路由钩子函数或组件，所以我们将返回一个 Promise，以便服务器能够等待所有的内容在渲染前就已经准备就绪。
   return new Promise((resolve, reject) => {
     const { app, router } = createApp()
 
--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,32 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+export function createStore(){
+    return new Vuex.Store({
+        state: {
+            homeData:[],
+            aboutData: []
+        },
+        mutations: {
+            SET_HOME_DATA(state, payload){
+               state.homeData = payload
+            },
+            SET_ABOUT_DATA(state, payload){
+               state.aboutData = payload
+            }
+        },
+        actions: {
+            fetchHomeData({ commit }){
+                const data = [1,2,3]
+                commit('SET_HOME_DATA', data)
+                console.log('home fetch')
+            },
+            fetchAboutData({ commit }){
+                const data = [4,5,6]
+                commit('SET_ABOUT_DATA', data)
+                console.log('about fetch')
+            }
+        },
+        
+    })
+}
